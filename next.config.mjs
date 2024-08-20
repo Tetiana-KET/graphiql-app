e9bad8b38a75de58ed0dc36dceee44549bb47d22
tatiana <belangelphone@gmail.com>
2024-08-20 10:38:54 +0300

    chore: set up project environment

---
+++ next.config.mjs
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: './dist',
+
+  webpack: (config, options) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (
+          moduleLoader.loader !== undefined &&
+          moduleLoader.loader.includes('css-loader') &&
+          typeof moduleLoader.options.modules === 'object'
+        ) {
+          moduleLoader.options = {
+            ...moduleLoader.options,
+            modules: {
+              ...moduleLoader.options.modules,
+              exportLocalsConvention: 'camelCase',
+            },
+          };
+        }
+      });
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
