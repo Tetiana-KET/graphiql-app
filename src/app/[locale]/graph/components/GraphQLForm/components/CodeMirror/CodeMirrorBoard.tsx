3beefdeec2ff96ae73c1e632978bca31b6a169d6
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-08-22 21:24:16 +0300

    feat: correct code mirror board

--- src/app/[locale]/graph/components/GraphQLForm/components/CodeMirror/CodeMirrorBoard.tsx
+++ src/app/[locale]/graph/components/GraphQLForm/components/CodeMirror/CodeMirrorBoard.tsx
@@ -4,60 +4,18 @@ import { GraphQLFormData } from '@/models/FormInterfaces';
 import { javascript } from '@codemirror/lang-javascript';
 import { Button } from '@nextui-org/react';
 import CodeMirror from '@uiw/react-codemirror';
-import { useCallback, useState } from 'react';
 import { UseFormRegister, UseFormSetValue } from 'react-hook-form';
+import { useCodeMirrorBoard } from '../../hooks/useCodeMirrorBoard';
 
-interface CodeMirrorBoardProps {
+export interface CodeMirrorBoardProps {
   register: UseFormRegister<GraphQLFormData>;
   setValue: UseFormSetValue<GraphQLFormData>;
 }
 
-const DEFAULT_GRAPHQL_QUERY = `
-    query GetUser {
-      user(id: "1") {
-        id
-        name
-        email
-      }
-    }
-  `;
-
 function CodeMirrorBoard({ register, setValue }: CodeMirrorBoardProps) {
-  const [query, setQuery] = useState(DEFAULT_GRAPHQL_QUERY);
-
-  const onChange = useCallback(
-    (value: string) => {
-      setQuery(value);
-      setValue('query', JSON.stringify(query));
-    },
-    [query, setValue],
-  );
-
-  const manualPrettify = (qglQuery: string): string => {
-    return qglQuery
-      .split('\n')
-      .map((line) => line.trim())
-      .map((line) => {
-        if (line.startsWith('}')) {
-          return `  ${line}`;
-        }
-        if (line.startsWith('query') || line.startsWith('mutation')) {
-          return line;
-        }
-        return `    ${line}`;
-      })
-      .join('\n');
-  };
-
-  const prettifyCode = useCallback(() => {
-    try {
-      const prettified = manualPrettify(query);
-      setQuery(prettified);
-      setValue('query', prettified);
-    } catch (error) {
-      console.error('Failed to prettify the code:', error);
-    }
-  }, [query, setValue]);
+  const { query, handleBoardValue, prettifyCode } = useCodeMirrorBoard({
+    setValue,
+  });
 
   return (
     <div>
@@ -65,7 +23,7 @@ function CodeMirrorBoard({ register, setValue }: CodeMirrorBoardProps) {
         {...register('query')}
         value={query}
         extensions={[javascript({ jsx: true })]}
-        onChange={onChange}
+        onChange={handleBoardValue}
       />
       <div className="flex gap-2">
         <Button
