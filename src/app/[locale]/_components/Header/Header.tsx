879bbd67a7b9af127b8606bb7ec08738db52454e
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-08-26 12:00:57 +0300

    feat: start header

--- src/app/[locale]/_components/Header/Header.tsx
+++ src/app/[locale]/_components/Header/Header.tsx
@@ -1,18 +1,52 @@
 import initTranslations from '@/app/i18n';
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react';
+import Link from 'next/link';
 import styles from './Header.module.scss';
 import LanguageChanger from '../LanguageChanger/LanguageChanger';
+import NavBarLinks from '../NavBar/NavBarLinks';
+import { Logo } from './Logo';
+import ClientHeaderWrapper from '../ClientHeaderWrapper/ClientHeaderWrapper';
 
 const i18nNamespaces = ['common'];
 
 async function Header({ locale }: { locale: string }) {
   const { t } = await initTranslations(locale, i18nNamespaces);
   return (
-    <header className={styles.header} data-testid="header">
-      <div className={styles.headerContainer}>
-        <h1>{t('headerHeader')} 🐈</h1>
+    <ClientHeaderWrapper>
+      <Navbar isBordered className={styles.header} data-testid="header">
+        <NavbarBrand>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </NavbarBrand>
+        <NavBarLinks />
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Button as={Link} color="primary" href="auth" variant="flat">
+              {t('signIn')}
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button
+              as={Link}
+              color="primary"
+              href="auth"
+              variant="flat"
+              className="hidden lg:flex"
+            >
+              {t('signOut')}
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
         <LanguageChanger />
-      </div>
-    </header>
+      </Navbar>
+    </ClientHeaderWrapper>
   );
 }
 export default Header;
---
+++ src/consts/navigationList.ts
@@ -0,0 +1,5 @@
+export const navigationList = [
+  { id: '1', title: 'REST Client ', path: '/rest' },
+  { id: '2', title: 'GraphiQL', path: '/graph' },
+  { id: '3', title: 'History', path: '/history' },
+];
